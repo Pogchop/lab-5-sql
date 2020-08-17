206114250518345ad581b132db01ba73983f5a94
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2020-08-17 11:22:05 +0700

    Add files via upload

---
+++ lab5t2.sql
@@ -0,0 +1,12 @@
+INSERT INTO PhongBan VALUES ('MaPB', 'lmao', 12, 'd', 'njee');
+GO
+INSERT INTO PhongBan VALUES ('MqPB', 'lmfao', 14, 'r', 'tsm');
+GO
+INSERT INTO PhongBan VALUES ('MePB', 'lmao', 13, 'e', 'nj');
+GO
+INSERT INTO PhongBan VALUES ('MrPB', 'lol', 11, 'f', 'nje');
+GO
+INSERT INTO PhongBan VALUES ('MtPB', 'kekw', 10, 'u', 'jee');
+GO
+INSERT INTO PhongBan VALUES ('MuPB', 'lamo', 18, 'x', 'nee');
+GO
